63118f16b93e67502afaf64fb5bc35cf4ba15fe1
kurosanex <kurosanex5010@me.com>
2015-05-16 21:19:22 +0900

    Initial Awake page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
     
-    it { should have_content('Sample App') }
+    it { should have_content('Team Awake') }
     it { should have_title("Ruby on Rails Tutorial Sample App") }
     it { should_not have_title('| Home') }
   end
